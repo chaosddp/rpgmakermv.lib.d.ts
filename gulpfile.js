5dbe0ab211a2e9b1127bb0168ba19b0ef1f3a1bb
chaos yu <wb-yupingchao@alibaba-inc.com>
2015-10-28 13:30:00 +0800

    add bitmap and pixi.d.ts

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 
+
 gulp.task("default", function(){
 	
+	// copy reference definition files
+	gulp.src("ref/*.d.ts")
+		.pipe(gulp.dest("output"));
+		
 	// here to merge files to a single one
-	
-	gulp.src("src/*.d.ts")
+	gulp.src(["src/ref.txt","src/*.d.ts"])
 		.pipe(concat("rpgmakermv.lib.d.ts"))
-		.pipe(gulp.dest("output"))
+		.pipe(gulp.dest("output"));
 });
